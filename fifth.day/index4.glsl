a091890620744154d48acba92e519ae7de436bb2
Bloodorange0313 <koudai7272@gmail.com>
2019-03-18 22:13:42 +0900

    glsl入門

---
+++ fifth.day/index4.glsl
@@ -0,0 +1,10 @@
+precision mediump float;
+uniform float t; // time
+uniform vec2  r; // resolution
+
+void main(void){
+    vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y); // 正規化
+    p += vec2(cos(t * 5.0), sin(t)) * 0.5;
+    float l = 0.1 + abs(sin(t)) / length(p);
+    gl_FragColor = vec4(vec3(l), 1.0);
+}
